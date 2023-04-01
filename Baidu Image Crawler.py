87b236a7c585b79b44a8d89b99a445c069160447
HYK-X <62224761+HYK-X@users.noreply.github.com>
2023-04-02 04:37:41 +0800

    Add files via upload

--- Baidu Image Crawler.py
+++ Baidu Image Crawler.py
@@ -1,76 +1,78 @@
-import os
-import random
 import requests
+import json
+import os
 import time
+import random
 from concurrent.futures import ThreadPoolExecutor
-from requests.exceptions import JSONDecodeError
+from fake_useragent import UserAgent
+#fake_useragent库规避反爬虫
+ua = UserAgent()
 
-def download_image(img_url, file_path, headers):
+
+def download_image(url, filepath):
     try:
-        img_data = requests.get(img_url, headers=headers).content
-        with open(file_path, "wb") as f:
-            f.write(img_data)
-            print(f"图片保存成功: {file_path}")
-            return True
-    except:
-        print(f"图片保存失败: {file_path}")
-        return False
-#max_workers为线程数，pause_interval暂停时间，max_retry_count失败次数
-def baidu_image_crawler(keyword, num_images, max_workers=16, pause_interval=60, max_retry_count=3):
-    url = f"https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&word={keyword}"
-    #请求user_agents规避反爬虫
-    user_agents = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15",
-    ]
+        response = requests.get(url, headers={'User-Agent': ua.random}, timeout=10)
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+        print(f"图片下载成功: {filepath}")
+    except Exception as e:
+        print(f"图片下载失败: {url}, 错误信息: {e}")
+
+#pause_interval暂停时间，max_retry_count失败次数，proxies代理
+def baidu_image_crawler(keyword, num_images, max_retry_count=5, pause_interval=60, proxies=None):
+    url_template = "https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord={keyword}&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=&hd=&latest=&copyright=&word={keyword}&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn={pn}&rn=30&gsm=1e&1594447993172="
 
     if not os.path.exists(keyword):
-        os.mkdir(keyword)
+        os.makedirs(keyword)
 
     count = 0
+    success_count = 0
     retry_count = 0
-    while count < num_images:
-        headers = {"User-Agent": random.choice(user_agents)}
 
-        try:
-            response = requests.get(f"{url}&pn={count}", headers=headers)
-            data = response.json()
-        except JSONDecodeError:
-            print("JSON 解析错误，跳过当前请求")
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        while count < num_images:
+            pn = count
+            url = url_template.format(keyword=keyword, pn=pn)
 
-            retry_count += 1
-            if retry_count >= max_retry_count:
-                print(f"暂停 {pause_interval} 秒以规避反爬虫程序")
-                time.sleep(pause_interval)
-                retry_count = 0
-            continue
-
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            success_count = 0
-            for img_info in data.get("data", []):
-                img_url = img_info.get("thumbURL")
-                if img_url:
-                    file_path = os.path.join(keyword, f"{count}.jpg")
-                    result = executor.submit(download_image, img_url, file_path, headers)
-                    if result.result():
-                        success_count += 1
-                        count += 1
-
-                if count >= num_images:
-                    break
-
-            if success_count == 0:
+            try:
+                response = requests.get(url, headers={'User-Agent': ua.random}, timeout=10, proxies=proxies)
+                response.raise_for_status()
+                data = response.json()
+            except json.JSONDecodeError as e:
+                print(f"JSON解析错误: {e}")
                 retry_count += 1
                 if retry_count >= max_retry_count:
                     print(f"暂停 {pause_interval} 秒以规避反爬虫程序")
                     time.sleep(pause_interval)
                     retry_count = 0
-                else:
-                    retry_count = 0
+                continue
+            except requests.exceptions.RequestException as e:
+                print(f"请求错误: {e}")
+                retry_count += 1
+                if retry_count >= max_retry_count:
+                    print(f"暂停 {pause_interval} 秒以规避反爬虫程序")
+                    time.sleep(pause_interval)
+                retry_count = 0
+                continue
+            else:
+                retry_count = 0
+
+            for item in data.get("data", []):
+                if success_count >= num_images:
+                    break
+
+                img_url = item.get("thumbURL")
+                if img_url:
+                    filepath = os.path.join(keyword, f"{success_count}.jpg")
+                    executor.submit(download_image, img_url, filepath)
+                    success_count += 1
+
+            count += 30
+            time.sleep(random.uniform(1, 3))
+
 
 if __name__ == "__main__":
-    keyword = "手机"#此处更改关键词
+    keyword = "猫"#此处更改关键词
     num_images = 10000#此处更改参数
     baidu_image_crawler(keyword, num_images)
+
